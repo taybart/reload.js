b19c842f211f349abc978aa46125967cdcc7b18c
Taylor Bartlett <taybart@gmail.com>
2025-03-02 13:43:10 -0700

    fix: don't wait for dom to load

--- src/reload.js
+++ src/reload.js
@@ -1,32 +1,26 @@
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    let failed = false
-    let reload = false
-    setTimeout(() => {
-      setInterval(() => {
-        const controller = new AbortController()
-        const id = setTimeout(() => controller.abort(), 250)
-        fetch('/hc', {
-          signal: controller.signal,
-        })
-          .then((res) => {
-            if (res.status === 200 && failed && !reload) {
-              console.log('reload')
-              reload = true
-              window.location.reload()
-            }
-            clearTimeout(id)
-          })
-          .catch((e) => {
-            console.log(e)
-            if (!failed) {
-              failed = true
-              console.log('failed')
-            }
-          })
-      }, 500)
-    }, 1000)
-  },
-  { once: true },
-)
+let failed = false
+let reload = false
+setTimeout(() => {
+  setInterval(() => {
+    const controller = new AbortController()
+    const id = setTimeout(() => controller.abort(), 250)
+    fetch('/hc', {
+      signal: controller.signal,
+    })
+      .then((res) => {
+        if (res.status === 200 && failed && !reload) {
+          console.log('reload')
+          reload = true
+          window.location.reload()
+        }
+        clearTimeout(id)
+      })
+      .catch((e) => {
+        console.log(e)
+        if (!failed) {
+          failed = true
+          console.log('failed')
+        }
+      })
+  }, 500)
+}, 1000)
